6298153e58600be19d82c30211c195d75eb795b4
hiyokomanjiu <hiyokomanjiu@gmail.com>
2018-05-13 19:59:59 +0900

    add files

---
+++ main.py
@@ -0,0 +1,92 @@
+# 
+
+import os
+import sys
+from PyQt5 import QtWidgets
+from mainWindow import Ui_MainWindow
+
+# matplotlib
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import numpy as np
+from skimage import io
+from skimage.filters import threshold_otsu
+from skimage.measure import label, regionprops
+
+class Application(QtWidgets.QMainWindow):
+    def __init__(self,parent=None):
+        super(Application, self).__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        # enable drag and drop
+        self.setAcceptDrops(True)
+
+        # set matplotlib FigureCanvas
+        self.fig = Figure()
+        self.FigureCanvas = FigureCanvas(self.fig)
+        self.ax1 = self.fig.add_subplot(1,1,1)
+        self.ax1.set_axis_off()
+        self.ui.imageLayout.addWidget(self.FigureCanvas)
+
+        # button
+        self.ui.pushButton_save.clicked.connect(self.onClick_save)
+
+    def dragEnterEvent(self, e):
+        if e.mimeData().hasFormat('text/uri-list'):
+            e.accept()
+        else:
+            e.ignore()
+    
+    def dropEvent(self, e):
+        # ファイルパスを取得する
+        urls = e.mimeData().text().split("\n")
+        self.filepath = urls[0][8:]
+
+        # display image
+        self.image = io.imread(self.filepath)
+        self.binimage = self.image > threshold_otsu(self.image)
+
+        # labeling
+        self.label = label(self.binimage)
+        for l in regionprops(self.label):
+            # add item
+            item = QtWidgets.QListWidgetItem(str(l.label))
+            self.ui.listWidget.addItem(item)
+        
+        self.plotImage()
+
+        self.ui.listWidget.itemSelectionChanged.connect(self.onItemSelectionChanged)
+
+    def plotImage(self):
+        # image
+        self.viewImage = np.zeros(self.image.shape)
+        for item in self.ui.listWidget.selectedItems():
+            l = int(item.text())
+            self.viewImage[self.label == l] = 1
+
+        self.ax1.clear()
+        self.ax1.imshow(self.viewImage, cmap='gray')
+        self.ax1.set_axis_off()
+
+        # redraw
+        self.FigureCanvas.draw()
+    
+    def onClick_save(self):
+        # show file chooser
+        savename = QtWidgets.QFileDialog.getSaveFileName(self, 'save file', os.path.dirname(self.filepath), "PNG Image (*.png)")
+        if savename[0]:
+            print(savename[0])
+            io.imsave(savename[0], self.viewImage * 255)
+    
+    def onItemSelectionChanged(self):
+        self.plotImage()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Application()
+    window.show()
+    sys.exit(app.exec_())
---
+++ mainWindow.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\mainWindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setAcceptDrops(False)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.widget_2 = QtWidgets.QWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy)
+        self.widget_2.setObjectName("widget_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.widget_2)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.listWidget = QtWidgets.QListWidget(self.widget_2)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listWidget.sizePolicy().hasHeightForWidth())
+        self.listWidget.setSizePolicy(sizePolicy)
+        self.listWidget.setMinimumSize(QtCore.QSize(150, 0))
+        self.listWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.listWidget.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.listWidget.setSelectionRectVisible(False)
+        self.listWidget.setObjectName("listWidget")
+        self.horizontalLayout.addWidget(self.listWidget)
+        self.widget_3 = QtWidgets.QWidget(self.widget_2)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget_3.sizePolicy().hasHeightForWidth())
+        self.widget_3.setSizePolicy(sizePolicy)
+        self.widget_3.setObjectName("widget_3")
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout(self.widget_3)
+        self.horizontalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        self.imageLayout = QtWidgets.QHBoxLayout()
+        self.imageLayout.setObjectName("imageLayout")
+        self.horizontalLayout_4.addLayout(self.imageLayout)
+        self.horizontalLayout.addWidget(self.widget_3)
+        self.verticalLayout.addWidget(self.widget_2)
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.widget.sizePolicy().hasHeightForWidth())
+        self.widget.setSizePolicy(sizePolicy)
+        self.widget.setMinimumSize(QtCore.QSize(0, 80))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.widget)
+        self.horizontalLayout_2.setContentsMargins(-1, 5, -1, 5)
+        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
+        spacerItem = QtWidgets.QSpacerItem(563, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.pushButton_save = QtWidgets.QPushButton(self.widget)
+        self.pushButton_save.setObjectName("pushButton_save")
+        self.horizontalLayout_2.addWidget(self.pushButton_save)
+        self.verticalLayout.addWidget(self.widget)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 38))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton_save.setText(_translate("MainWindow", "Save"))
+
